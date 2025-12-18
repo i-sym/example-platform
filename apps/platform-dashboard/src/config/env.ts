d37afb2ffae7377549fa64ee049ca2475fa2709c
Ivan Symotiuk <ivan.symotiuk@gmail.com>
2025-12-18 02:05:01 +0200

    fix: ensure consistent env search token

--- apps/platform-dashboard/src/config/env.ts
+++ apps/platform-dashboard/src/config/env.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 export const EnvSchema = z.object({
     // [Hint]: You can setup crawler to look for the token in next lines
-    // begin ENVS
+    // begin_ENV
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-   BACKEND_URL: z.string().url(),
-    // end ENVS
+    BACKEND_URL: z.string().url(),
+    // end_ENV
 });
 
 export const env = EnvSchema.parse(process.env);
